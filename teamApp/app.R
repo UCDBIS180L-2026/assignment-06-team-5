19594092d4374434b46e6b9532843ec19bb79c41
dwdeq@ucdavis.edu <exouser@image-build-instance-ubuntu-24-1773443964010129633.js2local>
2026-04-23 15:08:25 -0700

    added drop-downs to UI

--- teamApp/app.R
+++ teamApp/app.R
@@ -1,14 +1,43 @@
 library(shiny)
 # other libraries here
 
+rice <- read_csv("RiceDiversity.44K.MSU6.Phenotypes.csv")
+
+rice <- as_tibble(rice)
+summary(rice)
+
 # data loading and one-time processing here
 
 
 # Define UI for application 
-ui <- fluidPage( #create the overall page
-    #UI code here
+ui <- fluidPage(#create the overall page
+  
+  # Application title
+  titlePanel("Rice Data by Population"),
+  
+  # Some helpful information
+  helpText("This application allows you to see the correlation between",
+           "traits based on population.  Please use the drop down menu", 
+           "below to choose traits for plotting"),
+  
+  # Sidebar with a radio box to input which trait will be plotted
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("trait1", #the input variable that the value will go into
+                   "Choose the first trait to display:",
+                   colnames(rice)
+      ),
+      selectInput("trait2", #the input variable that the value will go into
+                  "Choose the second trait to display:",
+                  colnames(rice)
+      )),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("pointPlot")
+    )
   )
-
+)
 
 # Define server logic 
 server <- function(input, output) {
