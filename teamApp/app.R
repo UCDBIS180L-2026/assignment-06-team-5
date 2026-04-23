37d23ec9a8fa4bd130271c75affa7a3be5d848a5
Majken <mohorton@ucdavis.edu>
2026-04-23 15:12:54 -0700

    majken commit base plot

--- teamApp/app.R
+++ teamApp/app.R
@@ -2,7 +2,7 @@ library(shiny)
 # other libraries here
 
 # data loading and one-time processing here
-
+rice <- read_csv("./teamApp/RiceDiversity.44K.MSU6.Phenotypes.csv")
 
 # Define UI for application 
 ui <- fluidPage( #create the overall page
@@ -12,7 +12,11 @@ ui <- fluidPage( #create the overall page
 
 # Define server logic 
 server <- function(input, output) {
-  # server code here
+  
+  rice %>% 
+    ggplot(rice, mapping = aes(x = trait1,
+                               y = trait1)) +
+    geom_point()
 }
 
 # Run the application 
